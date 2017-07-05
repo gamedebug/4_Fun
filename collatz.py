e07af5fbb86f3300a59b00f401a5af655ad091aa
陆翊司 <luyisi1982@gmail.com>
2017-07-05 14:49:30 +0800

    Fix bugs

--- collatz.py
+++ collatz.py
@@ -15,7 +15,7 @@ while True:
             if int(n) == 1:
                 print ("经过%s次运算得到%s" % (count, n))
                 break
-    elif n == "exit" or "quit":
+    elif n == "exit":
         break
     else:
         print "输入错误，请重新输入!!!"
