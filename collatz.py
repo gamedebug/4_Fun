ad4d8fae85db2c51f492c240b70818cbf3f6e1f8
陆翊司 <luyisi1982@gmail.com>
2017-07-05 14:31:22 +0800

    update

--- collatz.py
+++ collatz.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
 count = 0
 
 while True:
-    n = raw_input("Enter a number: ")
+    n = raw_input("请输入一个数字: ")
     if n.isdigit():
-        break
+        while True:
+            if int(n)%2 == 0:
+                n = int(n)/2
+            else:
+                n = int(n)*3+1
+            count = count + 1
+            if int(n) == 1:
+                print ("经过%s次运算得到%s"%(count, n))
+                break
+    elif n == "exit":
+        break 
     else:
-        print "INPUT ERROR!!!"
+        print "输入错误，请重新输入!!!"
 
-while True:
-    if int(n)%2 == 0:
-        n = int(n)/2
-    else:
-        n = int(n)*3+1
-    count = count + 1
-    if int(n) == 1:
-        break
-print n
-print count
