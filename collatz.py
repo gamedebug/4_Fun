33321313631320b24ed86b10018b0fbde7c83836
陆翊司 <luyisi1982@gmail.com>
2017-07-05 14:38:42 +0800

    Fix with PEP8 check

--- collatz.py
+++ collatz.py
@@ -7,16 +7,17 @@ while True:
     n = raw_input("请输入一个数字: ")
     if n.isdigit():
         while True:
-            if int(n)%2 == 0:
+            if int(n) % 2 == 0:
                 n = int(n)/2
             else:
                 n = int(n)*3+1
             count = count + 1
             if int(n) == 1:
-                print ("经过%s次运算得到%s"%(count, n))
+                print ("经过%s次运算得到%s" % (count, n))
                 break
     elif n == "exit":
-        break 
+        break
     else:
         print "输入错误，请重新输入!!!"
+
 
