a1b41e7aefa35994e95ecac1796413eca3dc0dc8
gamedebug <gamedebug@vip.qq.com>
2017-12-14 23:18:03 +0800

    Update

---
+++ days.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import datetime
+
+date1 = raw_input("请输入起始日期（Y/M/D）：")
+date2 = raw_input("请输入结束日期（Y/M/D）：")
+
+d1 = datetime.datetime(int(date1.split('/')[0]), 
+                       int(date1.split('/')[1]), 
+                       int(date1.split('/')[2]))
+d2 = datetime.datetime(int(date2.split('/')[0]), 
+                       int(date2.split('/')[1]), 
+                       int(date2.split('/')[2]))
+print "总天数为: " + str((d2-d1).days)
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+function qyy {
+    echo "Hello world!"
+}
