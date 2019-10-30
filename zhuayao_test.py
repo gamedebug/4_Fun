77cf8821401f177f7bf53155274932cb814e2026
Louis <gamedebug@vip.qq.com>
2019-10-31 00:41:30 +0800

    Create zhuayao_test.py

---
+++ zhuayao_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+
+x = int(raw_input("请输入取样空间: "))
+for i in range(0,x):
+    b = np.random.randint(1,33,5)
+    if len(set(b)) == 1:
+        print str(b) + "高资！"
+#    else:
+#        print str(b) + "渣渣！"
