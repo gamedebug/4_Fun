fc8dc6975239616da3cb6f08d8cf84eed4fe1da7
陆翊司 <gamedebug@vip.qq.com>
2025-08-15 03:52:39 +0100

    Update days.py

--- days.py
+++ days.py
@@ -12,4 +12,4 @@ d1 = datetime.datetime(int(date1.split('/')[0]),
 d2 = datetime.datetime(int(date2.split('/')[0]), 
                        int(date2.split('/')[1]), 
                        int(date2.split('/')[2]))
-print "总天数为: " + str((d2-d1).days)
+print("总天数为: " + str((d2-d1).days))
