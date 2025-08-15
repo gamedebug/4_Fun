f7d77f1ff11d5ecb08757ad4ee32ac1393bb42a8
陆翊司 <gamedebug@vip.qq.com>
2025-08-15 03:56:18 +0100

    Fix Python 2 print statements and ignore __pycache__

--- gcd_lcm.py
+++ gcd_lcm.py
@@ -25,13 +25,13 @@ while True:
     if isinstance(a, int):
         break
     else:
-        print "Input ERROR!!!"
+        print("Input ERROR!!!")
 
 while True:
     b = input("Input another number: ")
     if isinstance(b, int):
         break
     else:
-        print "Input ERROR!!!"
-print "The GCD is %s" %gcd(a, b)
-print "The LCM is %s" %lcm(a, b)
+        print("Input ERROR!!!")
+print("The GCD is %s" %gcd(a, b))
+print("The LCM is %s" %lcm(a, b))
--- vpngate.py
+++ vpngate.py
@@ -13,7 +13,7 @@ __email__ = "andrea.lazzarotto@gmail.com"
 
 
 if len(sys.argv) != 2:
-    print 'usage: ' + sys.argv[0] + ' [country name | country code]'
+    print('usage: ' + sys.argv[0] + ' [country name | country code]')
     exit(1)
 country = sys.argv[1]
 
@@ -22,7 +22,7 @@ if len(country) == 2:
 elif len(country) > 2:
     i = 5 # long name for country
 else:
-    print 'Country is too short!'
+    print('Country is too short!')
     exit(1)
 
 try:
@@ -32,29 +32,29 @@ try:
     labels[0] = labels[0][1:]
     servers = [s for s in servers[2:] if len(s) > 1]
 except:
-    print 'Cannot get VPN servers data'
+    print('Cannot get VPN servers data')
     exit(1)
 
 desired = [s for s in servers if country.lower() in s[i].lower()]
 found = len(desired)
-print 'Found ' + str(found) + ' servers for country ' + country
+print('Found ' + str(found) + ' servers for country ' + country)
 if found == 0:
     exit(1)
 
 supported = [s for s in desired if len(s[-1]) > 0]
-print str(len(supported)) + ' of these servers support OpenVPN'
+print(str(len(supported)) + ' of these servers support OpenVPN')
 # We pick the best servers by score
 winner = sorted(supported, key=lambda s: float(s[2].replace(',','.')), reverse=True)[0]
 
-print "\n== Best server =="
+print("\n== Best server ==")
 pairs = zip(labels, winner)[:-1]
 for (l, d) in pairs[:4]:
-    print l + ': ' + d
+    print(l + ': ' + d)
 
-print pairs[4][0] + ': ' + str(float(pairs[4][1]) / 10**6) + ' MBps'
-print "Country: " + pairs[5][1]
+print(pairs[4][0] + ': ' + str(float(pairs[4][1]) / 10**6) + ' MBps')
+print("Country: " + pairs[5][1])
 
-print "\nLaunching VPN..."
+print("\nLaunching VPN...")
 _, path = tempfile.mkstemp()
 
 f = open(path, 'w')
@@ -75,4 +75,4 @@ except:
         pass
     while x.poll() != 0:
         time.sleep(1)
-    print '\nVPN terminated'
+    print('\nVPN terminated')
